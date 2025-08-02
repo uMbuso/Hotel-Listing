e643f06ed1a3dce064331b6d923589cb928e9286
mbuso nkosi <mbusonkosi26@gmail.com>
2025-08-02 15:34:14 +0200

    Initial commmit
    Projevt setup and serilog initialization

---
+++ HotelListing/Program.cs
@@ -0,0 +1,45 @@
+
+using Serilog;
+
+namespace HotelListing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs/hotellisting.txt", rollingInterval: RollingInterval.Day, 
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
