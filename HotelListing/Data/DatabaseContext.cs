984b8a6645d24a538f193b6248edc05ce353706d
mbuso nkosi <mbusonkosi26@gmail.com>
2025-08-14 10:03:07 +0200

    Seeding initial data into database

--- HotelListing/Data/DatabaseContext.cs
+++ HotelListing/Data/DatabaseContext.cs
@@ -12,5 +12,22 @@ namespace HotelListing.Data
 
         public DbSet<Country> Countries { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>()
+                .HasData(
+                    new Country { Id = 1, Name = "South Africa", ShortName = "RSA" },
+                    new Country { Id = 2, Name = "Canada", ShortName = "CA" },
+                    new Country { Id = 3, Name = "Mexico", ShortName = "MX" }
+                );
+
+            modelBuilder.Entity<Hotel>()
+                .HasData(
+                    new Hotel { Id = 1, Name = "Arcadia", Address = "Address 1", Rating = 4.5, CountryId = 1 },
+                    new Hotel { Id = 2, Name = "Sunnyside", Address = "Address 2", Rating = 5, CountryId = 2 },
+                    new Hotel { Id = 3, Name = "Highveld", Address = "Address 3", Rating = 2, CountryId = 3 }
+                );
+        }
     }
 }
---
+++ HotelListing/Migrations/20250814080159_seedingData.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "South Africa", "RSA" },
+                    { 2, "Canada", "CA" },
+                    { 3, "Mexico", "MX" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Address 1", 1, "Arcadia", 4.5 },
+                    { 2, "Address 2", 2, "Sunnyside", 5.0 },
+                    { 3, "Address 3", 3, "Highveld", 2.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
