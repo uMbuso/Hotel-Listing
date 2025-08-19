d9cc5ba5241e7c083931ae4fcf2d8ec1a7dd360d
mbuso nkosi <mbusonkosi26@gmail.com>
2025-08-19 12:34:46 +0200

    Adds IRepository

---
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using HotelListing.Entities;
+
+namespace HotelListing.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+        
+    }
+}
