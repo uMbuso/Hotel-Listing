b4d5ab202901b341b5466c5d8bd031ee7e779573
mbuso nkosi <mbusonkosi26@gmail.com>
2025-09-26 12:45:05 +0200

    Adds get action method for countries

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,36 @@
+﻿using HotelListing.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                return Ok(countries);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(GetCountries)}");
+                return StatusCode(500, "Internal server error. Please try again later.");
+            }
+
+        }
+    }
+}
