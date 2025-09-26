70c8f7eaff25a3d7a535e00f172aab1127ee288d
mbuso nkosi <mbusonkosi26@gmail.com>
2025-09-26 18:30:12 +0200

    Adds Hotel controller and getAll action method

---
+++ HotelListing/Controllers/HotelController.cs
@@ -0,0 +1,34 @@
+﻿using HotelListing.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> GetHotels()
+        {
+            try
+            {
+                var hotels = await _unitOfWork.Hotels.GetAll();
+                return Ok(hotels);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(GetHotels)}");
+                return StatusCode(500, "Internal server error. Please try again later.");
+            }
+        }
+    }
+}
